65e4c7fb81d4c8073260b82c71e1427301b7ca1f
vigneshwaran <vigneshvenketasan@gmail.com>
2025-12-05 21:00:35 +0530

    LocalStorage

--- utils/localStorage.js
+++ utils/localStorage.js
@@ -1,3 +1,12 @@
 export const saveUser = (data) => {
-    localStorage.setItem("authData" ,JSON.stringify())
+    localStorage.setItem("authData" ,JSON.stringify(data))
 }
+
+export const loadUser = () => {
+  const data = localStorage.getItem("authData");
+  return data ? JSON.parse(data) : null ;
+}
+
+export const clearUser = () => {
+    localStorage.removeItem("authData")
+};
