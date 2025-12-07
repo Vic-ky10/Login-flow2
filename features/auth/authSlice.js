da846bc4a3b5a0d1751b51e12d5d4640c4df08ce
vigneshwaran <vigneshvenketasan@gmail.com>
2025-12-07 20:33:48 +0530

    created the authslice

--- features/auth/authSlice.js
+++ features/auth/authSlice.js
@@ -1,51 +1,67 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
-import { loadUser } from "../../utils/localStorage";
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { saveUser, loadUser, clearUser } from "../../utils/localStorage";
 
 const initialState = loadUser() || {
-    user :null,
-    token:null,
-    loading : false,
-    error : null
-}
+  user: null,
+  token: null,
+  loading: false,
+  error: null,
+};
 
-// fake api login
-
-const fakeLogin = ({email , password}) => {
-    new Promise((resolve ,reject)=> {
-        setTimeout(()=> {
-            if(password === "123456"){
-                resolve({
-                    user:{name :"Vicky" , email},
-                    token :"jwt-token-xyz"
-                });
-                
-            }else{
-                   reject("Wrong password") 
-                }
-        },1000)
-    })
-}
+// Fake Api login
+const fakeLogin = ({ email, password }) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (password === "123456") {
+        resolve({
+          user: { name: "Vicky", email },
+          token: "jwt-token-xyz",
+        });
+      } else {
+        reject("Wrong password!");
+      }
+    }, 1000);
+  });
 
 export const loginUser = createAsyncThunk(
-    "auth/loginUser",
-     async (data , {rejectWithValue }) => {
-     try{
-        return await fakeLogin(data);
-     }catch(err){
-        return rejectWithValue(err)
-     }
+  "auth/loginUser",
+  async (data, { rejectWithValue }) => {
+    try {
+      return await fakeLogin(data);
+    } catch (err) {
+      return rejectWithValue(err);
     }
+  }
 );
 
 const authSlice = createSlice({
-    name :"auth",
-    initialState,
-    reducers:{
-        logout :(state) => {
-            state.user = null ;
-            state.action = null;
-            clearUser();
-        }
-    }
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      clearUser();
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        saveUser(action.payload);
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { logout } = authSlice.actions;
+export default authSlice.reducer;
